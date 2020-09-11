c5bcb9fbc555cc036f31ac777cc88125de83939c
Pablo Quiceno <jpquicen@bancolombia.com.co>
2020-09-11 12:34:50 -0500

    Base project was implemented

---
+++ src/main/java/wolox/training/WoloxTrainingApplication.java
@@ -0,0 +1,13 @@
+package wolox.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoloxTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoloxTrainingApplication.class, args);
+	}
+
+}
